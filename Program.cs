641dce20dd72be072191185e05df0ceed23cc86b
Rupesh Dhabade <rupesh.dhabade@gmail.com>
2024-06-20 22:55:03 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project3Csharp
+{
+    internal class Program
+    {
+        public void Add()
+        {
+            int a = 10, b = 20, c = a + b;
+            Console.WriteLine(c);
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Add();
+
+            Console.ReadLine();
+        }
+    }
+}
